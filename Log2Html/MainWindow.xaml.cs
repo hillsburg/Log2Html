e40eb56dc7a0011a2a02f34e753783892dce5469
hillsburg <hillsburg@qq.com>
2023-07-26 17:49:21 +0800

    feat: add menu

--- Log2Html/MainWindow.xaml.cs
+++ Log2Html/MainWindow.xaml.cs
@@ -46,6 +46,12 @@ namespace Log2Html
         {
             vm.SaveSettings(_configFilePath);
         }
+
+        private void Open_Html_File(object sender, RoutedEventArgs e)
+        {
+            //System.Diagnostics.Process.Start(txt_html_path.Text);
+            System.Diagnostics.Process.Start(@"D:\\CODE\\NEWest\\automotiveota\\code\\GTS.MaxSign_bin\\Result\\GPS L1\\004008001_20230724105829\\Test20230725110454.html");
+        }
     }
 }
 
